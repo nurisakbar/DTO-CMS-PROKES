0818d9ea3be71ff20b47eb49daae1f260638560f
Nuris Akbar <nuris.akbar@gmail.com>
2021-11-10 22:55:41 +0700

    Update navbar.blade.php

--- src/resources/views/layouts/frontend/navbar.blade.php
+++ src/resources/views/layouts/frontend/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-white">
+{{-- <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-white">
     <div class="container">
       <a class="navbar-brand" href="/">
         <img src="{{asset('frontend')}}/assets/img/logokemkes.png" alt="Kementerian Kesehatan" >
@@ -28,4 +28,39 @@
         </ul>
       </div><!-- /collapse navbar -->
     </div><!-- /container -->
-  </nav><!-- /navbar -->
+  </nav><!-- /navbar --> --}}
+
+
+
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="/">
+            <img src="{{asset('frontend')}}/assets/img/logokemkes.png">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">Beranda <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Tentang</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Program</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Karir</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">FAQ</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
