6b4682b1286985121c2d4c0edb89e4614b687f6f
Nuris Akbar <nuris.akbar@gmail.com>
2021-11-10 22:49:13 +0700

    Update layout.blade.php

--- src/resources/views/layout.blade.php
+++ src/resources/views/layout.blade.php
@@ -23,7 +23,7 @@
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
         <h5 class="my-0 mr-md-auto font-weight-normal"><a href="/"><img src="{{ asset('images/logokemkes.png')}}"></a></h5>
         
-        <nav class="my-2 my-md-0 mr-md-3">
+        <nav class="navbar navbar-inverse navbar-fixed-top my-2 my-md-0 mr-md-3">
             <a class="p-2 text-dark" href="/">Home</a>
             <a class="p-2 text-dark" href="#">Tentang</a>
             <a class="p-2 text-dark" href="#">Program</a>
